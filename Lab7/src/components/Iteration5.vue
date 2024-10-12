e687cc0e40c70f31a9b037502f6e91884281a7b4
Tim Rabisch <tim.rabisch@gmx.de>
2024-10-12 15:53:40 +0200

    done

--- Lab7/src/components/Iteration5.vue
+++ Lab7/src/components/Iteration5.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 const backColor=ref("aqua");
 
 const changeBackground=()=>{
-    
+
 
 }
 
@@ -12,10 +12,16 @@ const changeBackground=()=>{
 
 <template>
 <button class="cta" @click="changeBackground">Change Backgroundcolor</button>
+<div class="changer">
+    This Divs backgroundcolor is changed
+</div>
 </template>
 
 <style scope>
 *{
     background-color: backColor;
+}
+.changer{
+    background-color: aqua;
 }
 </style>
