9ee01786563cbfdfcd27cebd9400ca7c2172b31c
Tim Rabisch <tim.rabisch@gmx.de>
2024-10-12 15:32:58 +0200

    Done

--- Lab7/src/components/Iteration2.vue
+++ Lab7/src/components/Iteration2.vue
@@ -11,7 +11,7 @@ const stringToLowercase = computed(() => {
     <p>Mathematical Operation 2+2 = {{ 2+2 }}</p>
     <label for="string">String to be printed: </label>
     <input type="text" v-model="printString" id="string" placeholder="Enter a String">
-    <p>{{stringToLowercase(printString)}}</p>
+    <p>{{stringToLowercase}}</p>
 </template>
 
 <style scoped>
